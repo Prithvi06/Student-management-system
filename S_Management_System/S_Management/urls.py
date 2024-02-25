c305976f4f4f248e64e10d8075e0c9bdb53997f5
prithviraj <prithvirajsinghthakur74@gmail.com>
2024-02-25 14:49:42 +0530

    update urls

--- S_Management_System/S_Management/urls.py
+++ S_Management_System/S_Management/urls.py
@@ -14,53 +14,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from testapp import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    #url(r'^$',views.Admin_panel_view,name='admin_panel'),
-    url(r'^logout/', auth_views.LogoutView.as_view(template_name= 'registration/logged_out.html',next_page=None), name='logout'),
-    url(r'^login/', views.Login_view, name='login'),
-    url(r'^Mheadmaster/$',views.Headmaster_manage_view,name='Mheadmaster'),   #manage headmaster
-    url(r'^Rheadmaster/$',views.Headmaster_register,name='Rheadmaster'),       #register headmaster
-    url(r'^Rteacher/$',views.Teacher_register,name='Rteacher'),             #register teacher
-    url(r'^Rstudent/$',views.Student_register,name='Rstudent'),              #register teacher
-    url(r'^Mteacher/$',views.Teacher_manage_view,name='Mteacher'),  #manage teacher
-    url(r'^Mstudent/$',views.Student_manage_view,name='Mstudent'),  #manage student
-    url(r'^Mclassl/$',views.Manage_tclass_lecture,name='Mclassl'),    #manage t_class
-    url(r'^Aclass/$',views.Assign_class,name='Aclass'),                 #add class
-    url(r'^gclass/(?P<id>\d+)/$',views.get_class_view,name='gclass'),   #get class Detail
-    url(r'^tassign/$',views.Task_assign,name='tassign'),
-    url(r'^taskv/$',views.Task_assign_view,name='taskv'),
-    url(r'^tsk_Pstatus_assign/(?P<id1>\d+)/(?P<id2>\d+)/$',views.Student_task_Presult_view,name='tsk_Pstatus_assign'),
-    url(r'^tsk_Cstatus_assign/(?P<id1>\d+)/(?P<id2>\d+)/$',views.Student_task_Cresult_view,name='tsk_Cstatus_assign'),
-    url(r'^tsk_detail/(?P<id>\d+)/$',views.Task_status_detail,name='tsk_detail'),
-    url(r'^gmarks/(?P<id1>\d+)/(?P<id2>\d+)/$',views.give_marks,name='gmarks'),
+    path('admin/', admin.site.urls),
+    #path('$',views.Admin_panel_view,name='admin_panel'),
+    path('logout/', auth_views.LogoutView.as_view(template_name= 'registration/logged_out.html',next_page=None), name='logout'),
+    path('login/', views.Login_view, name='login'),
+    path('Mheadmaster/',views.Headmaster_manage_view,name='Mheadmaster'),   #manage headmaster
+    path('Rheadmaster/',views.Headmaster_register,name='Rheadmaster'),       #register headmaster
+    path('Rteacher/',views.Teacher_register,name='Rteacher'),             #register teacher
+    path('Rstudent/',views.Student_register,name='Rstudent'),              #register teacher
+    path('Mteacher/',views.Teacher_manage_view,name='Mteacher'),  #manage teacher
+    path('Mstudent/',views.Student_manage_view,name='Mstudent'),  #manage student
+    path('Mclassl/',views.Manage_tclass_lecture,name='Mclassl'),    #manage t_class
+    path('Aclass/',views.Assign_class,name='Aclass'),                 #add class
+    path('gclass/<int:id>',views.get_class_view,name='gclass'),   #get class Detail
+    path('tassign/',views.Task_assign,name='tassign'),
+    path('taskv/',views.Task_assign_view,name='taskv'),
+    path('tsk_Pstatus_assign/<int:id1>/<int:id2>/',views.Student_task_Presult_view,name='tsk_Pstatus_assign'),
+    path('tsk_Cstatus_assign/<int:id1>/<int:id2>/$',views.Student_task_Cresult_view,name='tsk_Cstatus_assign'),
+    path('tsk_detail/<int:id>',views.Task_status_detail,name='tsk_detail'),
+    path('gmarks/<int:id1>/<int:id2>/',views.give_marks,name='gmarks'),
 
-    url(r'^Asports/$',views.Sports_class,name='Add_sports'),         #add sports
-    url(r'^Msports/$',views.Manage_sports_view,name='Msports'),     #manage sports view
-    url(r'^hupdate/(?P<id>\d+)/$',views.hupdate_view,name='hupdate'),
-    url(r'^tupdate/(?P<id>\d+)/$',views.tupdate_view,name='tupdate'),
-    url(r'^supdate/(?P<id>\d+)/$',views.supdate_view,name='supdate'),
-    url(r'^cupdate/(?P<id>\d+)/$',views.cupdate_view,name='update'),
-    url(r'^department/$',views.department_view,name='department'),
-    url(r'^delete/(?P<id>\d+)/$',views.delete_view,name='delete'),
-    url(r'^cdelete/(?P<id>\d+)/$',views.cdelete_view,name='cdelete'),
+    path('Asports/',views.Sports_class,name='Add_sports'),         #add sports
+    path('Msports/',views.Manage_sports_view,name='Msports'),     #manage sports view
+    path('hupdate/<int:id>/',views.hupdate_view,name='hupdate'),
+    path('tupdate/<int:id>/',views.tupdate_view,name='tupdate'),
+    path('supdate/<int:id>/',views.supdate_view,name='supdate'),
+    path('cupdate/<int:id>/',views.cupdate_view,name='update'),
+    path('department/',views.department_view,name='department'),
+    path('delete/<int:id>/',views.delete_view,name='delete'),
+    path('cdelete/<int:id>/',views.cdelete_view,name='cdelete'),
 
-    url(r'^head_profile/$',views.head_profile_view,name='head_profile'),
-    url(r'^teacher_profile/$',views.teacher_profile_view,name='teacher_profile'),
-    url(r'^teacher_lecture/$',views.teacher_lecture_view,name='teacher_lecture'),
-    url(r'^student_profile/$',views.student_profile_view,name='student_profile'),
-    url(r'^student_lecture/$',views.student_lecture_view,name='student_lecture'),
+    path('head_profile/',views.head_profile_view,name='head_profile'),
+    path('teacher_profile/',views.teacher_profile_view,name='teacher_profile'),
+    path('teacher_lecture/',views.teacher_lecture_view,name='teacher_lecture'),
+    path('student_profile/',views.student_profile_view,name='student_profile'),
+    path('student_lecture/',views.student_lecture_view,name='student_lecture'),
 
 
-    url('reset_password/', auth_views.PasswordResetView.as_view(),name="reset_password"),
-    url('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(),name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 ]
 
 if settings.DEBUG:
